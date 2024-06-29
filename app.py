59761a0048aef0ecf1257471659724638d9a27be
YashGang <yashgangurde1610@gmail.com>
2024-06-29 06:12:25 +0530

    AssignmentA

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/home")
+def read_root():
+    return {"Hello": "MCC Students!!!"}
+
+@app.get("/about")
+def about():
+    return {"msg": "About Us"}
